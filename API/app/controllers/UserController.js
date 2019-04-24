0cdb3bbe2ce0b43457ebcc203799baf620f3efd8
Adekoreday <khord4eng@gmail.com>
2019-04-24 09:23:03 +0100

    feature(signup):add user controller to be able to signup [starts #165558749]

---
+++ API/app/controllers/UserController.js
@@ -0,0 +1,51 @@
+import jwt from 'jsonwebtoken';
+import UserService from '../services/UserService';
+import validPassword from '../../utils/validPassword';
+
+
+class UserController {
+
+    static async SignUp(req, res) {
+        const userdetails = req.body;
+        const userExist = await UserService.CheckifUserExist(userdetails.email);
+        let userList;
+        if (userExist === undefined) {
+            userdetails.permission = userdetails.isAdmin === true ? 'admin' : 'user';
+            const token = jwt.sign({ email: userdetails.email, permission: userdetails.permission }, process.env.SECRET_KEY, { expiresIn: '1h' });
+            userdetails.token = token;
+            //       userList = this.UserService.addNewUser(userdetails);
+            userList = await UserService.Signup(userdetails, res);
+        }
+        return res.status(userList === undefined ? 422 : 201).json({
+            status: userList === undefined ? 422 : 201,
+            Data: userList === undefined ? 'user already Exists' : userList,
+        });
+    }
+
+    static async SignIn(req, res) {
+        const userKey = req.body;
+        const userExist = await UserService.CheckifUserExist(userKey.email);
+        if (userExist !== undefined) {
+            const validatepass = validPassword(userKey.password, userExist.password);
+            if (validatepass) {
+                const tokens = jwt.sign({ email: userExist.email, id: userExist.id }, process.env.SECRET_KEY, { expiresIn: '1h' });
+                userExist.token = tokens;
+                return res.status(200).json({
+                    status: 200,
+                    Data: userExist,
+                });
+            }
+            return res.status(401).json({
+                status: 401,
+                msg: 'wrong password.........',
+            });
+
+        }
+        return res.status(404).json({
+            status: 404,
+            msg: 'user account not found',
+        });
+    }
+
+}
+export default UserController;
