f93d2392a4308b52922e0dd38e5940cc747460bc
Adekoreday <khord4eng@gmail.com>
2019-04-26 01:38:54 +0100

    feautre(transaction): credit and debit of user works [finishes #165626772]

--- app/controllers/TransactionController.js
+++ app/controllers/TransactionController.js
@@ -3,18 +3,26 @@ import TransactionService from '../services/TransactionService';
 
 class TransactionController {
 
-    static async processTransaction(req, res) {
-        const transactionData = req.scafoldData;
-        const addedtransaction = await TransactionService.addNewTransaction(transactionData, res)
-        /*  res.status(addedtransaction === undefined ? 404 : 200).json({
+  static async processTransaction(req, res) {
+    const transactionData = req.scafoldData;
+    const addedtransaction = await TransactionService.addNewTransaction(transactionData, res);
+    /*  res.status(addedtransaction === undefined ? 404 : 200).json({
               status: addedtransaction === undefined ? 404 : 200,
               data: addedtransaction === undefined ? 'account oooooooo' : addedtransaction,
           }); */
-        res.status(200).json({
-            status: 200,
-            data: addedtransaction,
-        });
-    }
+    const mydata = {
+      TransactionId: addedtransaction.id,
+      accountNumber: req.params.accountNumber,
+      accountBalance: addedtransaction.newbalance,
+      amount: addedtransaction.amount,
+      cashier: addedtransaction.cashier,
+      Transactiontype: addedtransaction.type,
+    };
+    res.status(200).json({
+      status: 200,
+      data: mydata,
+    });
+  }
 
 }
 export default TransactionController;
